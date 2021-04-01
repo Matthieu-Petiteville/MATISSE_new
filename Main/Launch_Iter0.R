1f24f18976e6a69abf24a3725163bd2cbb0469c1
Matthieu-Petiteville <81426037+Matthieu-Petiteville@users.noreply.github.com>
2021-04-01 14:37:25 +0200

    Creation of a script to launch the full iter=0 steps

---
+++ Main/Launch_Iter0.R
@@ -0,0 +1,32 @@
+# Objectif : lancer tous les étapes de la première itération d'un scénario à partir d'une boucle (jusqu'au renvoi dans IMACLIM-3ME)
+
+
+# Launch ------------------------------------------------------------------
+M_home <- gsub("\\\\","/",Sys.getenv("MATISSE_HOME"))
+M_data <- gsub("\\\\","/",Sys.getenv("MATISSE_DATA"))
+source(paste(M_home,"/Common/tools.R",sep=""))
+source(paste(M_home,"/Common/default_values.r",sep=""))
+source(paste(M_home,"/Main/Matisse_Loop.r",sep=""))
+current_log <- CreateLogFile()
+
+# Loop on scenarios/horizon/classement/redistribution -------------------------------------------------------
+
+for (scenario in scenario_v){
+  for (horizon in horizon_v){
+    for (scenario_classement in scenario_classement_v){
+      for (redistribution in redistribution_v){
+        
+        #Run the full Matisse_Loop, steps 1 to 5, for Iter=0
+        #Logs into current_log
+        Iter=0
+        sink(file = current_log,append = TRUE)
+        RunMatisseLoop(1:5)
+        sink()
+        
+      }
+    }
+  }
+}
+
+
+
