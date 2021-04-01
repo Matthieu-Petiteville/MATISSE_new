1f9d0b361899d9e5c9fdbae85d10751dad9afc72
Matthieu-Petiteville <81426037+Matthieu-Petiteville@users.noreply.github.com>
2021-04-01 14:39:06 +0200

    Change to handle the new folder archi

--- Main/1_Launch.R
+++ Main/1_Launch.R
@@ -2,70 +2,62 @@
 
 
 # Launch ------------------------------------------------------------------
+M_home <- gsub("\\\\","/",Sys.getenv("MATISSE_HOME"))
+M_data <- gsub("\\\\","/",Sys.getenv("MATISSE_DATA"))
+source(paste(M_home,"/Common/tools.R",sep=""))
+source(paste(M_home,"/Common/default_values.r",sep=""))
 
 
+# Loop on scenarios/horizon/classement/redistribution -------------------------------------------------------
 
-for (scenario in c("AMS")){
-  for (horizon in c(2035)){
-    for (scenario_classement in c("Optimal_co2","Optimal_ener")){
-      for (redistribution in c("decile")){
-        
+for (scenario in scenario_v){
+  for (horizon in horizon_v){
+    for (scenario_classement in scenario_classement_v){
+      for (redistribution in redistribution_v){
         
         Iter=0
         print(paste("Scenario : ",scenario,", horizon : ", horizon, ", scenario technique :",scenario_classement, " ,scenario redistributif :",redistribution,sep=""))
-        
-        beg <- proc.time()
-        print(strptime(Sys.time(),format="%Y-%m-%d %H:%M:%S"))
-        
         ####
         # Step 1
         ####
         
-        source("D:/CIRED/Projet_Ademe/MATISSE/Step_1_Mise_echelle/0_mise_forme_data_3ME.R")
-        source("D:/CIRED/Projet_Ademe/MATISSE/Step_1_Mise_echelle/1.1_mise_echelle_revenus.R")
-        source("D:/CIRED/Projet_Ademe/MATISSE/Step_1_Mise_echelle/2.1_retrocession_carbon_tax.R")
+        source(paste(M_home,"/Step_1_Mise_echelle/0_mise_forme_data_3ME.R",sep=""))
+        source(paste(M_home,"/Step_1_Mise_echelle/1.1_mise_echelle_revenus.R",sep=""))
+        source(paste(M_home,"/Step_1_Mise_echelle/2.1_retrocession_carbon_tax.R",sep=""))
         
         ####
         # Step 2
         ####
         
-        source("D:/CIRED/Projet_Ademe/MATISSE/Step_2_Microsimulation/1.1_Microsimulation.R")
-        source("D:/CIRED/Projet_Ademe/MATISSE/Step_2_Microsimulation/2.1_evolution_conso_energie.R")
-        
-        
+        source(paste(M_home,"/Step_2_Microsimulation/1.1_Microsimulation.R",sep=""))
+        source(paste(M_home,"/Step_2_Microsimulation/2.1_evolution_conso_energie.R",sep=""))
+
         ####
         # Step 3
         ####
         
-        source("D:/CIRED/Projet_Ademe/MATISSE/Step_3_Technical_Change/3_1_TC_DPE/3_1_1_Achat_neuf_horizon.R")
-        source("D:/CIRED/Projet_Ademe/MATISSE/Step_3_Technical_Change/3_1_TC_DPE/3_1_2_Achat_2010_horizon-1.R")
-        source("D:/CIRED/Projet_Ademe/MATISSE/Step_3_Technical_Change/3_1_TC_DPE/3_1_3_Rehab_2010_horizon.R")
-        source("D:/CIRED/Projet_Ademe/MATISSE/Step_3_Technical_Change/3_1_TC_DPE/3_1_4_Bascule_A.R")
-        
-        
-        # source("D:/CIRED/Projet_Ademe/MATISSE/Step_3_Technical_Change/3_2_TC_VE/3_2_1_VE_classement_horizon.R")
-        source("D:/CIRED/Projet_Ademe/MATISSE/Step_3_Technical_Change/3_2_TC_VE/3_2_2_VE_2010_horizon.R")
+        source(paste(M_home,"/Step_3_Technical_Change/3_1_TC_DPE/3_1_1_Achat_neuf_horizon.R",sep=""))
+        source(paste(M_home,"/Step_3_Technical_Change/3_1_TC_DPE/3_1_2_Achat_2010_horizon-1.R",sep=""))
+        source(paste(M_home,"/Step_3_Technical_Change/3_1_TC_DPE/3_1_3_Rehab_2010_horizon.R",sep=""))
+        source(paste(M_home,"/Step_3_Technical_Change/3_1_TC_DPE/3_1_4_Bascule_A.R",sep=""))
+        source(paste(M_home,"/Step_3_Technical_Change/3_2_TC_VE/3_2_2_VE_2010_horizon.R",sep=""))
         
         ####
         # Step 4
         ####
         
-        source("D:/CIRED/Projet_Ademe/Publication/Subventions_menages/MATISSE_Subv/Step_4_Reweighting/1_contraintes_macro.R")
-        source("D:/CIRED/Projet_Ademe/Publication/Subventions_menages/MATISSE_Subv/Step_4_Reweighting/2_contraintes_micro.R")
-        source("D:/CIRED/Projet_Ademe/Publication/Subventions_menages/MATISSE_Subv/Step_4_Reweighting/3_pondr.R")
+        source(paste(M_home,"/Step_4_Reweighting/1_contraintes_macro.R",sep=""))
+        source(paste(M_home,"/Step_4_Reweighting/2_contraintes_micro.R",sep=""))
+        source(paste(M_home,"/Step_4_Reweighting/3_pondr.R",sep=""))
         
         ####
         # Step 5
         ####
         
-        source("D:/CIRED/Projet_Ademe/MATISSE/Step_5_export_IMACLIM/1_export_to_IMACLIM.R")
-        
-        
-        end <- proc.time() - beg
+        source(paste(M_home,"/Step_5_export_IMACLIM/1_export_to_IMACLIM.R",sep=""))
+
         print(paste(paste("Scenario : ",scenario,", horizon : ", horizon, ", scenario technique :",scenario_classement, " ,scenario redistributif :",redistribution,sep="")," : DONE (",end[3]/60," min)",sep=""))
-        
-        
-        
+
       }
     }
   }
