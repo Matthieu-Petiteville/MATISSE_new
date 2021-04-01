e8fd956e059351c211afc59cf7350cbdde27bd53
Matthieu-Petiteville <81426037+Matthieu-Petiteville@users.noreply.github.com>
2021-04-01 14:39:45 +0200

    Removing comments

--- Step_1_Mise_echelle/2.1_retrocession_carbon_tax.R
+++ Step_1_Mise_echelle/2.1_retrocession_carbon_tax.R
@@ -3,7 +3,8 @@
 # LIBRARY -----------------------------------------------------------------
 library(pracma)
 library(tidyverse)
-# setwd("D:/Stage_Petiteville/Projet_Ademe/MATISSE/")
+source(paste(M_home,"/Step_1_Mise_echelle/2.2_fonction_retrocession_carbon_tax.R",sep=""))
+
 
 # DATA --------------------------------------------------------------------
 
@@ -12,14 +13,9 @@ load(paste(M_data,"/Output/Projet_Ademe/",scenario,"/",horizon,"/IMACLIM.RData",
 load(paste(M_data,"/Output/Projet_Ademe/",scenario,"/EMS.RData",sep=""))
 load(paste(M_data,"/Output/Initial format/menage_forme.RData",sep=""))
 
-
-# source("Step_1_Mise_echelle/2.3_emission_gap_2010.R")
 # pour gagner du temps : 
 facteur_dec1 <-  2.201544
 facteur_tuu0 <-  3.32953
-
-source(paste(M_home,"/Step_1_Mise_echelle/2.2_fonction_retrocession_carbon_tax.R",sep=""))
-
 
 # Taxe Carbone ------------------------------------------------------------
 
@@ -39,15 +35,12 @@ TCO<-as.numeric(IMACLIM %>%
 
 
 # Appel Fonction ----------------------------------------------------------
-
 menage_echelle <- retrocession_carbon_tax(TCO,TCO_tot,menage_echelle)
 
 
 # Save --------------------------------------------------------------------
 
 save(menage_echelle,file=paste(M_data,"/Output/Projet_Ademe/",scenario,"/",horizon,"/",scenario_classement,"/",redistribution,"/Iteration_0/Output/menage_echelle_1.RData",sep=""))
-
-
 
 
 # Success -----------------------------------------------------------------
