76438d88ebafeb99eb4e433051e238c9b8438c28
Matthieu-Petiteville <81426037+Matthieu-Petiteville@users.noreply.github.com>
2021-04-06 13:54:21 +0200

    Launcher that uses the Parallelize process, useful for Iter = 0

---
+++ Main/Launch_Parallelize_Iter0.R
@@ -0,0 +1,52 @@
+# Objectif : lancer tous les étapes de la première itération d'un scénario à partir d'une boucle (jusqu'au renvoi dans IMACLIM-3ME)
+
+
+# Launch ------------------------------------------------------------------
+M_home <- gsub("\\\\","/",Sys.getenv("MATISSE_HOME"))
+M_data <- gsub("\\\\","/",Sys.getenv("MATISSE_DATA"))
+source(paste(M_home,"/Common/tools.R",sep=""))
+source(paste(M_home,"/Common/default_values.r",sep=""))
+source(paste(M_home,"/Main/Matisse_Loop.r",sep=""))
+source(paste(M_home,"/Parallelize/ParallelizeMatisse.r",sep=""))
+MaxRscript = 4
+
+CurrentRScript = 0
+# Loop on scenarios/horizon/classement/redistribution -------------------------------------------------------
+for (scenario in scenario_v){
+  for (horizon in horizon_v){
+    for (scenario_classement in scenario_classement_v){
+      for (redistribution in redistribution_v){
+        
+        CleanParallelizeFolder()
+        CurrentRScript <- CountActiveRScripts()
+        
+        #Boucle pour éviter d'avoir trop de scripts ouverts
+        while(CurrentRScript>=MaxRscript){
+          Sys.sleep(10)
+          CleanParallelizeFolder()
+          CurrentRScript <- CountActiveRScripts()
+        } 
+        
+        
+        #Run the full Matisse_Loop, steps 1 to 5, for Iter=0
+        #Logs into current_log
+        Iter=0
+        AddLogs("PARAL","Running script for step 1")
+        Parallelize_Matisse_Loop(step_to_run =  2, ForceRerun = FALSE)
+
+      }
+    }
+  }
+}
+
+
+
+StandardValues4Test <- function(){
+
+#Sets up default values for the 4 parameters of Matisse to allow a 'defaut' run
+scenario <- scenario_v[1]
+horizon <- horizon_v[1]
+scenario_classement <- scenario_classement_v[1]
+redistribution <- redistribution_v[1]
+
+}
