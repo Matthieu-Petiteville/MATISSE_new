a2100a70c4304b24e091058bd2bb4a2ebf222d22
Matthieu-Petiteville <81426037+Matthieu-Petiteville@users.noreply.github.com>
2021-04-16 14:36:21 +0200

    Added default_values

--- MATISSE_init.R
+++ MATISSE_init.R
@@ -1,4 +1,5 @@
 M_home <- gsub("\\\\","/",Sys.getenv("MATISSE_HOME"))
 M_data <- gsub("\\\\","/",Sys.getenv("MATISSE_DATA"))
 source(paste(M_home,"/Common/tools.R",sep=""))
+source(paste(M_home,"/Common/default_values.r",sep=""))
 
