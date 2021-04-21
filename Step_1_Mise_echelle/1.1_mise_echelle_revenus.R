fd20e024b28e8c2c922f19afad1b0f73d98b36a8
Matthieu-Petiteville <matthieu.petiteville@gmail.com>
2021-04-21 10:38:10 +0200

    updating to new mise_echelle_revenu function

--- Step_1_Mise_echelle/1.1_mise_echelle_revenus.R
+++ Step_1_Mise_echelle/1.1_mise_echelle_revenus.R
@@ -2,13 +2,12 @@
 # LIBRARY -----------------------------------------------------------------
 library(tidyverse)
 source(paste(M_home,"/Common/tools.R",sep=""))
+source(paste(M_home,"/Step_1_Mise_echelle/1.2_fonction_mise_echelle_revenus.R",sep=""))
 
 # DATA --------------------------------------------------------------------
 
-
 ## MICRO
 load(MatisseFiles$menage_forme_rd)
-source(paste(M_home,"/Step_1_Mise_echelle/1.2_fonction_mise_echelle_revenus.R",sep=""))
 
 ## MACRO
 load(MatisseFiles$IMACLIM_rd)
@@ -17,19 +16,12 @@ load(MatisseFiles$Threeme_rd)
 
 # Appel Fonction  ---------------------------------------------------------
 
-Iter=0
-menage_echelle <- mise_echelle_revenu(FC,menage_forme,Iter)
+menage_echelle <- mise_echelle_revenu(FC,menage_forme)
 
 
 # Save files --------------------------------------------------------------
 
 save(menage_echelle,file=MatisseFiles$menage_echelle_1_1_rd)
-
-
-
-# Next Step ---------------------------------------------------------------
-# Rétrocession de la taxe carbone
-
 
 
 # Clean -------------------------------------------------------------------
