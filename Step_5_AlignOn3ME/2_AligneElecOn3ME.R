ca6aa933d76d49c86953ce5c5f3815e479c8a777
Matthieu-Petiteville <81426037+Matthieu-Petiteville@users.noreply.github.com>
2021-07-21 11:15:38 +0200

    Aligning Elec on forfait standard

--- Step_5_AlignOn3ME/2_AligneElecOn3ME.R
+++ Step_5_AlignOn3ME/2_AligneElecOn3ME.R
@@ -6,19 +6,22 @@
 library(tidyverse)
 library(readxl)
 library(scales)
+source(paste(M_home,"/Step_3_Technical_Change/3_1_TC_DPE/Econometrie_solde_budg_Logement.R",sep=""))
 
-initializeMatisseFiles()
 
 # Data --------------------------------------------------------------------
 
 if(!exists("align_bascule_on_3ME")){align_bascule_on_3ME <- F}
 
+#Dans le mode non forfait, on utilise la réduction de conso d'électricité du mode forfait
 if(redistribution != "forfait"){
   load(MatisseFiles$menage_calibre_ele_forfait_rd)
   load(MatisseFiles$menage_calibre_forfait_rd)
   rapport_depElec <- mean(menage_echelle_calibre_ele$dep_Elec / menage_echelle_calibre$dep_Elec, na.rm = T)
+  rm(menage_echelle_calibre, menage_echelle_calibre_ele)
 }
 
+load(MatisseFiles$menage_echelle_final_calibre_rd)
 
 if(align_bascule_on_3ME){
   load(MatisseFiles$menage_forme_rd)
@@ -54,42 +57,43 @@ if(align_bascule_on_3ME){
   Sum_Mat_2010 <- sum(menage_forme$dep_Elec * menage_forme$pondmen) / 10^6
   Sum_horizon_Mat <- sum(menage_echelle_calibre$dep_Elec * menage_echelle_calibre$pondmen)/ FC$A02 / 10^6
   
-  while(abs(Sum_horizon_Mat/Sum_Mat_2010 - Sum_3ME[2,"sum_Me"]/Sum_3ME[1,"sum_Me"])>0.001){
-    
-    #Facteur efficacité elec
+  #Facteur efficacité elec
+  if(redistribution == "forfait"){
     effic_ele <- as.numeric(Sum_3ME[2,"sum_Me"]/Sum_3ME[1,"sum_Me"]*Sum_Mat_2010/Sum_horizon_Mat)
-    print(paste("Effic_ele =",effic_ele))
-    print(paste("3MEho =",Sum_3ME[2,"sum_Me"]))
-    print(paste("3ME2010 =",Sum_3ME[2,"sum_Me"]))
-    print(paste("Mat2010 =",Sum_Mat_2010))
-    print(paste("Matho =",Sum_horizon_Mat))
-    
-    
-    solde<- 
-      menage_echelle_calibre %>%
-      mutate(solde=0)%>% #<0 => économie si les dépenses équivalentes en élec sont plus faibles que les dépenses en gaz,fuel, solides,etc
-      select(ident_men,solde)
-  
-    #Ajustement efficacité domicile et solde
-    usages <- c("ECS", "chauff", "clim", "Cuisson", "ecl", "ElecSpe")
-    sources <- c("Elec")
-    for(so in sources){
-      for(us in usages){
-        my_col <- paste(so,us,sep="_")
-        if(my_col %in% names(menage_echelle_calibre)){
-          solde["solde"] <-  solde$solde + menage_echelle_calibre[,my_col] * as.numeric(effic_ele - 1)
-          menage_echelle_calibre[,my_col] <- menage_echelle_calibre[,my_col] * effic_ele
-        }      
-      }
-      menage_echelle_calibre[paste("dep",so,sep="_")] <- menage_echelle_calibre[paste("dep",so,sep="_")] * effic_ele
-    }
-    print(paste("Applied elec efficiency to calibrate to 3ME ", percent(1-effic_ele, accuracy = 0.01),sep=""))
-    
-    #Reventil
-    menage_echelle_calibre <- Ventil_solde(solde,menage_echelle_calibre,step="REHAB")
-    Sum_horizon_Mat <- sum(menage_echelle_calibre$dep_Elec * menage_echelle_calibre$pondmen)/ FC$A02 / 10^6
-    
+  }else{
+    effic_ele <- rapport_depElec
   }
+  print(paste("Effic_ele =",effic_ele))
+  print(paste("3MEho =",Sum_3ME[2,"sum_Me"]))
+  print(paste("3ME2010 =",Sum_3ME[2,"sum_Me"]))
+  print(paste("Mat2010 =",Sum_Mat_2010))
+  print(paste("Matho =",Sum_horizon_Mat))
+  
+  
+  solde<- 
+    menage_echelle_calibre %>%
+    mutate(solde=0)%>% #<0 => économie si les dépenses équivalentes en élec sont plus faibles que les dépenses en gaz,fuel, solides,etc
+    select(ident_men,solde)
+
+  #Ajustement efficacité domicile et solde
+  usages <- c("ECS", "chauff", "clim", "Cuisson", "ecl", "ElecSpe")
+  sources <- c("Elec")
+  for(so in sources){
+    for(us in usages){
+      my_col <- paste(so,us,sep="_")
+      if(my_col %in% names(menage_echelle_calibre)){
+        solde["solde"] <-  solde$solde + menage_echelle_calibre[,my_col] * as.numeric(effic_ele - 1)
+        menage_echelle_calibre[,my_col] <- menage_echelle_calibre[,my_col] * effic_ele
+      }      
+    }
+    menage_echelle_calibre[paste("dep",so,sep="_")] <- menage_echelle_calibre[paste("dep",so,sep="_")] * effic_ele
+  }
+  print(paste("Applied elec efficiency to calibrate to 3ME ", percent(1-effic_ele, accuracy = 0.01),sep=""))
+  
+  #Reventil
+  menage_echelle_calibre <- Ventil_solde(solde,menage_echelle_calibre,step="REHAB")
+  Sum_horizon_Mat <- sum(menage_echelle_calibre$dep_Elec * menage_echelle_calibre$pondmen)/ FC$A02 / 10^6
+
 }
 
 menage_echelle_calibre_ele <- menage_echelle_calibre
